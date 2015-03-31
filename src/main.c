4e51affe0294a204eecad2f06dbda0941b70e6ba
Jasper Nevens <jasper.nevens@fifthplay.com>
2015-03-31 21:47:49 +0200

    Add retry mechanism

--- src/main.c
+++ src/main.c
@@ -4,6 +4,8 @@
 
 #include "pi_2_dht_read.h"
 
+#define MAX_RETRIES 10
+
 void print_help(char *argv[])
 {
 	printf("Usage: %s DHT11|DHT22 gpio\n", argv[0]);
@@ -13,29 +15,36 @@ void print_help(char *argv[])
 int main(int argc, char *argv[])
 {
 	int sensor = DHT22;
+	int i = 0, rv = -1;
 	int pin = 2;
 	float humidity = -1.0;
 	float temperature = -99.0;
 
-	if(argc < 3) {
+	if (argc < 3) {
 		print_help(argv);
 	} else {
 		pin = atoi(argv[2]);
-		if(strcmp(argv[1], "DHT11") == 0) {
+		if (strcmp(argv[1], "DHT11") == 0) {
 			sensor = DHT11;
-		} else if(strcmp(argv[1], "DHT22") == 0 ) {
+		} else if (strcmp(argv[1], "DHT22") == 0) {
 			sensor = DHT22;
 		} else {
 			print_help(argv);
 		}
 	}
 
-	if(pi_2_dht_read(sensor, pin, &humidity, &temperature)) {
-		fprintf(stderr, "Failed reading dht!\n");
-	} else {
-		printf("Temperature: %f\n", temperature);
-		printf("Humidity: %f\n", humidity);
+	for (i = 0; i < MAX_RETRIES; i++) {
+		rv = pi_2_dht_read(sensor, pin, &humidity, &temperature);
+		if (rv == 0) {
+			printf("Temperature: %f\n", temperature);
+			printf("Humidity: %f\n", humidity);
+			break;
+		}
 	}
 
-	return 0;
+	if (i == MAX_RETRIES) {
+		fprintf(stderr, "Failed reading dht!\n");
+	}
+
+	return rv;
 }
